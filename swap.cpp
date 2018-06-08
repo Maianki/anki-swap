91672b44f61e6eefc1e0b6977e891fc8bc064f3e
Maianki <kumankit10@gmail.com>
2018-06-08 13:03:52 -0500

    swapping using two variables

--- swap.cpp
+++ swap.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int a = 5, b = 10, temp;
+    int a = 5, b = 10;
 
     cout << "Before swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
 
-    temp = a;
-    a = b;
-    b = temp;
+    a=a+b;
+    b=a-b;
+    a=a-b;
 
     cout << "\nAfter swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
